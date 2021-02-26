06c18b9c3bc8ce567bb085979e94ae520fdc25df
Harsh Verma <Harsh>
2021-02-26 15:43:30 +0530

    Reduce code by using library for Encryption

---
+++ README.md
@@ -0,0 +1,2 @@
+#Encryption 
+This is a tutorial Android application of encrypting messages
--- app/src/main/java/com/example/encryption/MainActivity.java
+++ app/src/main/java/com/example/encryption/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.encryption;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.aescrypt.AESCrypt;
 
 import java.security.MessageDigest;
 
@@ -24,38 +25,18 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = findViewById(R.id.editTextTextPersonName);
         TextView textView = findViewById(R.id.textView2);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = editText.getText().toString();
-                try {
-                    textView.setText(EncryptionMessage(message, "hello"));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        button.setOnClickListener(v -> {
+            String message = editText.getText().toString();
+            try
+            {
+                AESCrypt crypt = new AESCrypt("AES","1Hbfh667adfDEJ78");
+                textView.setText(crypt.encrypt(message));
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
-    }
 
-    private String EncryptionMessage (String message, String encryptionKey) throws Exception {
-        SecretKeySpec key = generateKey(message);
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte [] encVal = cipher.doFinal(message.getBytes());
-        String encryptedValue = Base64.encodeToString(encVal, Base64.DEFAULT);
-
-
-        return encryptedValue;
 
     }
-
-    private SecretKeySpec generateKey (String message) throws Exception {
-        final MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] bytes = message.getBytes();
-        digest.update(bytes, 0, bytes.length);
-        byte[] key = digest.digest();
-        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "ASE");
-        return secretKeySpec;
-    }
-
 }
